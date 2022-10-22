2ea52795a369c4c0076a75509bc36e67cd2f7a89
microbot101 <27mayim@gmail.com>
2022-10-22 10:53:10 -0700

    new changes on heroku app

--- website/__init_.py
+++ website/__init_.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-
 db = SQLAlchemy()
 DB_NAME = "backup.db"
 
@@ -11,8 +10,9 @@ DB_NAME = "backup.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hello102jodvkdnj'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = ' postgres://etdkimmgwgaqbb:4d7758cfc3ad6dbec865770a5f9d2290b6b3453f2eb6efe57c49977dc305a9d1@ec2-3-213-66-35.compute-1.amazonaws.com:5432/d9ll7h1dlputgs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config[
+    # 'SQLALCHEMY_DATABASE_URI'] = ' postgres://etdkimmgwgaqbb:4d7758cfc3ad6dbec865770a5f9d2290b6b3453f2eb6efe57c49977dc305a9d1@ec2-3-213-66-35.compute-1.amazonaws.com:5432/d9ll7h1dlputgs'
     db.init_app(app)
 
     from website.views import views
